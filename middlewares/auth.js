502c7e394b89827ec5c114e8f2df2cfad14da87f
Moonhee <o119a@naver.com>
2022-04-18 15:48:36 +0900

    user_1st updated

---
+++ middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken")
+const User = require("../schemas/user")
+const dotenv = require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  
+    const { authorization } = req.headers
+    const [tokenType, tokenValue] = authorization.split(' ');
+    // console.log(tokenType, tokenValue)
+    if (tokenType !== "Bearer") {
+        return res.status(401).send({
+            errorMessage: '로그인 후 이용하세요.'
+            
+        })
+    }
+
+    try {
+        const { email } = jwt.verify(tokenValue, `${process.env.KEY}`)
+        User.find({ email }).then(user => {
+            res.locals.user = user
+          
+            next()   
+        })
+    } catch (error) {
+        return res.status(401).json({
+            errorMessage: '토큰이 일치하지 않습니다.'
+            
+        })
+     
+    }
+    
+}
---
+++ schemas/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profileImg: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);

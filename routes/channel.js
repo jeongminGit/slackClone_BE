d2ce1dce6f65dbca4462be9b407d241af540a8a6
IcandoDeveloper <96129738+IcandoDeveloper@users.noreply.github.com>
2022-04-19 21:44:16 +0900

    채널, 채널 컨텐트 완성

--- routes/channel.js
+++ routes/channel.js
@@ -9,159 +9,7 @@ router.use(bodyParser.json());
 
 const moment = require("moment")
 
-
-
-
-
-
-
-// router.get("/post", async (req, res) => {
-//   const Posts = await Post.find();
-//   const Comments = await Post.find();
-//   res.json({list: Posts});
-//   console.log(list)
-
-// const oneChannel = 몽고DB 조회 결과 얻은 하나의 객체
-// const b = 몽고DB 조회 해서 이 채널에서 남긴 컨텐츠들 싹다 불러온거
-// a = [{"이름"}, {"일반"}, {"홍길동"}, {"추가한채널"}]
-// for(let i=0; i<a.length; i++) {
-//   oneChannel = a[1]
-//   {
-//     channelName: "추가한채널",
-//     connelHost: "닉네임",
-//     createdAt : "2022- -asd-fasdf",
-//   }
-//   mongoDB 조회
-//   const b = 몽고DB 조회 해서 이 채널에서 남긴 컨텐츠들 싹다 불러온거
-//   let queryResult = [{
-//     contentId: asdf,
-//     channelName: "추가한채널",
-//     ldkfjdl,
-//     .
-//     .
-//     .
-//   },
-//   {
-//     channelName: "추가한채널",
-//   }]
-//   let temp_html = {
-//     ...oneChannel,
-//     contentList: queryResult
-//   }
-// }
-// let searchInDB = [{
-//   contentId: asdf,
-//   channelName: "일반",
-//   ldkfjdl,
-// }, {
-//   contentId: dff,
-//   channelName: "일반",
-//   ldkfjdl,
-//   content: "적은내용",
-// }, {}]
-// let temp_html = {
-//   ...channelCommon,
-//   contentList: searchInDB,
-// }
-// let newDic = {
-//   channelName: channelCommon.channelName,
-//   connelHost: channelCommon.connelHost,
-//   createdAt: channelCommon.createdAt,
-//   contentList: searchInDB,
-// }
-// console.log(temp_html)
-// // 채널 하나의 모든 정보를 불러왔음
-// {
-//   channelName: "common",
-//   connelHost: "닉네임",
-//   createdAt : "2022- -asd-fasdf",
-//   contentList: [{
-//     contentId: asdf,
-//     channelName: "일반",
-//     ldkfjdl,
-//     ..
-//     .
-//     .
-//   }, {
-//     contentId: dff,
-//     channelName: "일반",
-//     ldkfjdl,
-//     content: "적은내용",
-//     ..
-//     .
-//   }, {}]
-// }
-
-
-
-
-
-
-
-
-
-
-
-//  //채널 사용자 조회
-// router.get('/:channelId/users', async (req, res) => {
-//   try {
-//       const { channelId } = req.params;
-//       const result = await allList.findAll({
-//           where: { channelId },
-//           include: [{
-//               // schemas: channel,
-//               order: [['createdAt', 'channelName', 'channelHost']]
-//           }]
-//       });
-
-//       res.json({ "ok": true, result });
-//   } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ ok: false, message: "채널 사용자 목록 불러오기를 실패하였습니다." });
-//   }
-// }); 
-
-
-
-
-
-// //채널 만들기
-// router.post('/channel/userId', async (req, res) => {
-//   try {
-//       const { channelName, createdAt, channelHost,allList } = req.body;
-//       const channel = await Channel.create({
-//           channelName,
-//           createdAt,
-//           channelHost,
-//           allList
-//       });
-//       const contentId = channel.id;
-//       for (let i = 0; i < allList.length; i++) {
-//           await allList.create({
-//               Id: allList[i],
-//               contentId,
-//           });
-//       }
-
-//       const result = await ChannelUserList.findAll({
-//           where: { channelId },
-//           include: [{
-//               model: User,
-//               order: [['createdAt', 'DESC']]
-//           }]
-//       });
-
-//       res.json({ ok: true, message: '채널등록을 성공하였습니다.' });
-//   } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ ok: false, message: '채널등록을 실패하였습니다.' });
-//   }
-// });
-
-
-
-
-
+//channel
 
 //채널 조회
 router.get("/channel/:userId", async (req, res) => {
@@ -211,90 +59,14 @@ router.post("/channel/channel",  async (req, res) => {
 //   "channelHost":"test3",
 //   "contentList":[]
 
-// test1 "channelId": "625e19aba43cad0547f9bd2d"
-// test1 "channelId": "625e19caa43cad0547f9bd2f"
 
-// test444 "createdAt": "2022_04_19_11:09", "channelId": "625e19e8a43cad0547f9bd31"
-// test444 "createdAt": "2022_04_19_11:09", "channelId": "625e19f5a43cad0547f9bd33"
-
-//"channelId": "625e15afa43cad0547f9bd25"
-//"channelId": "625e15c4a43cad0547f9bd27"
-//"channelId": "625e15cda43cad0547f9bd29"
-//"channelId": "625e15d4a43cad0547f9bd2b"
-
-
-
-
-
-
-
-
-// router.patch("/channel/:channelId",(async (req, res) => {
-//     try {
-//         const { channelId } = req.params;
-//         const { channelName } = req.body;
-
-       
-
-//         await channel.update({
-//             channelName
-//         }, {
-//             where: {
-//                 channelId: commentId
-//             },
-//         });
-//         res.json({ ok: true });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ ok: false, message: '댓글 수정를 실패하였습니다.' });
-//     }
-// }));
-
-
-
-
-
-
-
-// router.patch("/channel/:channelId",(async (req, res) => {
-//     const { channelId } = req.params;
-//     try {
-//         const channel = await channelId.findOne({ {channelId:channelId}});
-  
-//       await channel.update(
-//         {
-//           channelName: channelName,
-//         },
-//         {
-//            $set :{
-//             channelId:channelId
-//           },
-//         }
-//       );
-  
-//       res.status(204).json({
-//         ok: true,
-//         msg: "수정완료!",
-//       });
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-
-
-// )),
-
-
-
-//"channelName":"test300",
-//"channelId": "625e7cd90e67f200522fd618"
 
 
   //채널 수정 API
   router.patch("/channel/:channelId",(async (req, res) => {
     const { channelId } = req.params;
     const {channelName} = req.body;
-    const channel = await Channel.updateOne( {channelId:channelId},
+    const channel = await Channel.updateOne( {_id:channelId},
         {
             $set: {
                 channelName: channelName,
@@ -305,83 +77,8 @@ router.post("/channel/channel",  async (req, res) => {
     console.log({channelId})
     console.log({channelName})
     res.send();
-    //                      스키마
-    // const userId = res.locals.userId;
-
-
-    // upload(req, res, async (err) => {
-    //     const { content } = req.body;
-    //     let imgUrl;
-
-    //     if (req.file) {
-    //         imgUrl = req.file.path;
-    //     } else imgUrl = post.imgUrl;
-
-        // try {
-        //     if (err) {
-        //         return res.json({ success: false, err });
-        //     }
-        //     await channel.updateOne(
-        //         { channelId: channelId },
-        //         {
-        //             $set: {
-        //                 channelName: channelName,
-                        
-        //             },
-        //         }
-        //     );
-        //     res.send();
-        // } catch {
-        //     res.status(400).json({
-        //         errorMessage: "게시글 수정 중 오류 발생",
-        //     });
-        // }
-    // });
+   
 }));
-
-
-//   //게시글 수정 API
-//   router.patch("/:channelId/:contentId",(async (req, res) => {
-//     const { contentId } = req.params;
-//     const { channelName} =req.body;
-//     const content = await contentId.findOne({ contentId: contentId });
-//     // const userId = res.locals.userId;
-
-//     if (content.contentId !== contentId) {
-//         return res.status(412).json({
-//             errorMessage: "본인 게시글만 수정 가능합니다.",
-//         });
-//     }
-
-//     // upload(req, res, async (err) => {
-//     //     const { content } = req.body;
-//     //     let imgUrl;
-
-//     //     if (req.file) {
-//     //         imgUrl = req.file.path;
-//     //     } else imgUrl = post.imgUrl;
-
-//         try {
-//             if (err) {
-//                 return res.json({ success: false, err });
-//             }
-//             await content.updateOne(
-//                 { contentId: contentId },
-//                 { channelName },
-//             );
-//             res.send();
-//         } catch {
-//             res.status(400).json({
-//                 errorMessage: "게시글 수정 중 오류 발생",
-//             });
-//         }
-//     // });
-// }));
-
-//"channelId": "625e15afa43cad0547f9bd25"
-//"channelId": "625e15c4a43cad0547f9bd27"
-//"channelId": "625e15cda43cad0547f9bd29"
-//"channelId": "625e15d4a43cad0547f9bd2b"
 
 
 
@@ -401,9 +98,7 @@ router.delete("/channel/:channelId",  async (req, res) => {
 
 // //ContentInChannel
 
-//"channelId": "625e0e1577d96f9827860288"
-//"contentId": "0.9353774031667415"
-//채널 내용
+//컨텐츠 생성
 router.post("/:channelId/content", async (req, res) => {
     //authMiddleware, upload.single('imageUrl'),
     const { channelName, nickname, profileImg, content} = req.body;
@@ -414,20 +109,10 @@ router.post("/:channelId/content", async (req, res) => {
         
     //     { $addToSet: { contentList:`${contentId}` }}
     // );
-    
-
     const channel = await Channel.findOne({  _id:channelId })
     // console.log(channel,"dd")
-    channel.contentList.push(contentId) 
-        
-
-const doc = await channel.save()
-
-    
-
-
-    // console.log(doc,"여기!!")
-
+    channel.contentList.push(contentId)   
+    const doc = await channel.save()
     const createdAt = moment().format("YYYY_MM_DD_HH:mm");
     // console.log(content)
     const contentList =[await ChannelContent.create({
@@ -441,14 +126,8 @@ const doc = await channel.save()
         commentList:[]
     })];
     // console.log(contentList)
-  
-
     res.json({contentList});
-
-
-
 }),
-
 
 //  "channelId": "625d33e5838e2ff086197c83"
 // "channelContentId": "625d215cf1a740fa3136bc87"
@@ -463,33 +142,15 @@ const doc = await channel.save()
 
 
 
-
 //채널 내용 수정
 router.patch('/:channelId/:contentId', async (req, res) => {
 //   const {user} = res.locals
   const { contentId } = req.params;
-  const {  contents  } = req.body;
-  const targetContent = await content.findOne({ _id: contentId });
-
-  if (!targetContent) {
-      return res.status(400).json({
-          message: '다시 시도해주세요.',
-      });
-  } else {
-      if (contentId == targetContnent.contentId){//user.userId === targetContent.userId) {
-          await content.updateOne(
-              { _id: contentId },
-              { $set: { contents } }
-          );
+  const {  content  } = req.body;
+  await ChannelContent.updateOne({ contentId },{ $set: { content } });
           res.status(200).json({
               message: '게시물이 수정되었습니다.',
           });
-      } else {
-          return res.status(400).json({
-              message: '수정 권한이 없습니다.',
-          });
-      }
-  }
 });
 
 
@@ -498,16 +159,16 @@ router.patch('/:channelId/:contentId', async (req, res) => {
 //컨텐츠 삭제
 
 
-router.delete("/channelId/:contentId",  async (req, res) => {
+router.delete("/:channelId/:contentId",  async (req, res) => {
     //authMiddleware, upload.single('imageUrl'),
     const { contentId } = req.params;
-    await Channel.deleteOne({ contentId });
+    await ChannelContent.deleteOne({ contentId });
     res.send({ result: '삭제완료' });
   })
 
 
+//CommentInchannel
 
-//   625d93344729899b61c3c1e9
 //댓글 작성
 router.post("/:channelId/:contentId/comment", async (req, res) => {
     //authMiddleware, upload.single('imageUrl'),
@@ -527,7 +188,7 @@ router.post("/:channelId/:contentId/comment", async (req, res) => {
     res.json({commentList});
 }),
 
-//"channelCommentId": "625d2aede08f143773d004aa",
+// 댓글삭제
 
 router.delete("/channelId/:contentId/:commentId",  async (req, res) => {
     //authMiddleware, upload.single('imageUrl'),

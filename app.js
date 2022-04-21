8038f69e21b4c4625c0808ca54df7515e4f5493a
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-21 18:01:16 +0900

    error 주석처리

--- app.js
+++ app.js
@@ -81,13 +81,6 @@ chat.on("connection", (socket) => {
     // console.log(socket.id)
     socket.on("init", (payload) => {
         console.log("init 연결되었습니다~~~")
-        // exixtRoom = Chat.find({ roomName: room })
-    });
-    socket.on("join", ({ roomName: room, nickname: nickname }) => {
-        console.log("join 연결이되었습니다.")
-        // console.log("+++++++++++++++++++++++++++++++++", socket.rooms, "+++++++++++++++++++++++++++++++++")
-        // console.log(socket.id)
-        socket.join(room, nickname)
         Chat.find(function (err, result) {
             const arr = []
             if (result.length !== 0) {
@@ -97,13 +90,29 @@ chat.on("connection", (socket) => {
                 chat.to(socket.id).emit("receive message", arr.reverse())
             }
         })
-        // socket.emit("join", nickname, "님이 입장했습니다.");
-        // const { headers: { referer } } = req;
-        // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
-        // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+req, referer, roomId)
-        // socket.join(roomId);
-        // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+socket.request)
-    })
+        // exixtRoom = Chat.find({ roomName: room })
+    });
+    // socket.on("join", ({ roomName: room, nickname: nickname }) => {
+    //     console.log("join 연결이되었습니다.")
+    //     // console.log("+++++++++++++++++++++++++++++++++", socket.rooms, "+++++++++++++++++++++++++++++++++")
+    //     // console.log(socket.id)
+    //     socket.join(room, nickname)
+    //     // Chat.find(function (err, result) {
+    //     //     const arr = []
+    //     //     if (result.length !== 0) {
+    //     //         for (var i = result.length - 1; i >= 0; i--) {
+    //     //             arr.push({ nickname: result[i].nickname, message: result[i].message, createdAt: result[i].createdAt, profileImg: result[i].profileImg })
+    //     //         }
+    //     //         chat.to(socket.id).emit("receive message", arr.reverse())
+    //     //     }
+    //     // })
+    //     // socket.emit("join", nickname, "님이 입장했습니다.");
+    //     // const { headers: { referer } } = req;
+    //     // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
+    //     // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+req, referer, roomId)
+    //     // socket.join(roomId);
+    //     // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+socket.request)
+    // })
     socket.on("send message", (item) => {//send message 이벤트 발생
         // item: {nickname: String, msg: String, createdAt: String, profileImg: String}
         // console.log("+++++++++++++++++++++++++++++++++", room, "+++++++++++++++++++++++++++++++++")

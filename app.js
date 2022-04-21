3974be753a53ee8a50d605b0ceac67ecc012b031
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-21 10:36:49 +0900

    check

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ chat.on("connection", (socket)=> {
     })
     socket.on("send message", (item) => {//send message 이벤트 발생
         // console.log(item.nickname + " : " + item.message + " : " + item.createdAt);
-        socket.emit("receive message", { nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg});
+        socket.broadcast.emit("receive message", { nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg});
         // console.log(item.createdAt, item.profileImg)
         // var chat = new Chat({ nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg });
         // console.log("chat입니다----------------------@@@@@@@@@@", chat)

d866012e795d21576d26bc1f7976e0b6cd7bf9f0
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-21 10:21:10 +0900

    check

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ chat.on("connection", (socket)=> {
                 arr.push({nickname : result[i].nickname, message : result[i].message, createdAt : result[i].createdAt, profileImg: result[i].profileImg})
             }
         // console.log(arr, arr.reverse())
-        io.to(socket.id).emit("receive message", arr.reverse())
+        socket.emit("receive message", arr.reverse())
         console.log(arr)
         // const req = socket.request;
         // const { headers: { referer } } = req;
@@ -101,20 +101,20 @@ chat.on("connection", (socket)=> {
         // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+req, referer, roomId)
         });
     })
-    socket.on("send message", (item) => {//send message 이벤트 발생
-        // console.log(item.nickname + " : " + item.message + " : " + item.createdAt);
-        socket.emit("receive message", { nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg});
-        // console.log(item.createdAt, item.profileImg)
-        // var chat = new Chat({ nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg });
-        // console.log("chat입니다----------------------@@@@@@@@@@", chat)
-        console.log("item입니다----------------------!!!!!!!!!!", item)
-        Chat.create(item)
-        // const req = socket.request;
-        // const { headers: { referer } } = req;
-        // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
-        // console.log("#####################################################"+req, referer, roomId)
+    // socket.on("send message", (item) => {//send message 이벤트 발생
+    //     // console.log(item.nickname + " : " + item.message + " : " + item.createdAt);
+    //     socket.emit("receive message", { nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg});
+    //     // console.log(item.createdAt, item.profileImg)
+    //     // var chat = new Chat({ nickname: item.nickname, message: item.message, createdAt: item.createdAt, profileImg: item.profileImg });
+    //     // console.log("chat입니다----------------------@@@@@@@@@@", chat)
+    //     console.log("item입니다----------------------!!!!!!!!!!", item)
+    //     Chat.create(item)
+    //     // const req = socket.request;
+    //     // const { headers: { referer } } = req;
+    //     // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
+    //     // console.log("#####################################################"+req, referer, roomId)
        
-     });
+    //  });
     
 })
 

62578bb71f132c3eeb3646509086b9fb4e4a77db
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-21 16:53:35 +0900

    chat test

--- app.js
+++ app.js
@@ -75,46 +75,34 @@ app.use('/', indexRouter);
 // })
 const chat = io.of('/chat')
 chat.on("connection", (socket) => {
+    console.log("connection 연결이되었습니다.")
     // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",socket.rooms)
     // console.log(socket.id)
     socket.on("join", ({ roomName: room, nickname: nickname }) => {
+        console.log("join 연결이되었습니다.")
         console.log("+++++++++++++++++++++++++++++++++", socket.rooms, "+++++++++++++++++++++++++++++++++")
         console.log(socket.id)
-        if (socket.id) {
+        console.log(chat.emit("receive message"))
+        if (!chat.emit("receive message")) {
             socket.join(room, nickname)
             Chat.find(function (err, result) {
-                // console.log(socket.id)
                 const arr = []
                 for (var i = result.length - 1; i >= result.length - 10; i--) {
-                    // console.log(result[i])
-                    // var dbData = {nickname : result[i].nickname, message : result[i].message, createdAt : result[i].createdAt};
-                    // console.log(dbData.name, dbData.message)
-                    // io.emit("receive message", {nickname : result[i].nickname, message : result[i].message, createdAt : result[i].createdAt})
-                    // io.emit("receive message", {nickname : result[i].nickname, message : result[i].message, createdAt : result[i].createdAt, profileImg: result[i].profileImg})
                     arr.push({ nickname: result[i].nickname, message: result[i].message, createdAt: result[i].createdAt, profileImg: result[i].profileImg })
                 }
-                // console.log(arr, arr.reverse())
                 chat.to(socket.id).emit("receive message", arr.reverse())
             })
         }
-        chat.to(room).emit("onConnet", `${nickname} 님이 입장했습니다.`);
-        // send: 클라이언트가 메시지 보내는 이벤트
-        // socket.emit("여러분 만나서 반갑습니다")
-        // socket.send("여러분 만나서 반갑습니다")
-        // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + socket)
-        // console.log(chat)
-        // const req = socket.request;
+        chat.broadcast().emit("onConnet", `${nickname} 님이 입장했습니다.`);
         // const { headers: { referer } } = req;
         // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
         // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+req, referer, roomId)
         // socket.join(roomId);
         // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+socket.request)
-        console.log("연결이되었습니다.")
         socket.on("init", (payload) => {
             console.log("init 연결되었습니다~~~")
             exixtRoom = Chat.find({ roomName: room })
         });
-
         socket.on("send message", (item) => {//send message 이벤트 발생
             // item: {nickname: String, msg: String, createdAt: String, profileImg: String}
             console.log("+++++++++++++++++++++++++++++++++", room, "+++++++++++++++++++++++++++++++++")

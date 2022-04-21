f32d07b79ea10980f7624b8d5eef85cba75de9eb
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-04-21 17:56:34 +0900

    error 주석처리

--- app.js
+++ app.js
@@ -75,14 +75,10 @@ app.use('/', indexRouter);
 // })
 const chat = io.of('/chat')
 chat.on("connection", (socket) => {
+    
     console.log("connection 연결이되었습니다.")
     // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",socket.rooms)
     // console.log(socket.id)
-    socket.on("disconnect", () => {
-        socket.leave(room);
-        console.log("++++++++++++++++++++++++++++++++++++++++++++방을 나간거야!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        // chat.to(room).emit("onDisconnect", `${nickname} 님이 퇴장하셨습니다.`)
-    });
     socket.on("init", (payload) => {
         console.log("init 연결되었습니다~~~")
         // exixtRoom = Chat.find({ roomName: room })
@@ -118,7 +114,7 @@ chat.on("connection", (socket) => {
             message: item.message,
             createdAt: item.createdAt,
             profileImg: item.profileImg,
-            roomName: room
+            // roomName: room
         })
         saveChat.save()
         // socket.leave(room);;
@@ -128,6 +124,11 @@ chat.on("connection", (socket) => {
         // const roomId = referer.split('/')[referer.split('/').length - 1].replace(/\?.+/, '');
         // console.log("#####################################################"+req, referer, roomId)
     });
+    // socket.on("disconnect", () => {
+    //     socket.leave(room);
+    //     console.log("++++++++++++++++++++++++++++++++++++++++++++방을 나간거야!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    //     // chat.to(room).emit("onDisconnect", `${nickname} 님이 퇴장하셨습니다.`)
+    // });
 });
 
 
